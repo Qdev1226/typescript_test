632bb3e75ccd4662d2da37205af36e0a68f45711
Jay <wmeng013@gmail.com>
2023-08-18 10:45:45 +1000

    third

--- test.js
+++ test.js
@@ -1,2 +1,10 @@
 var message = "Hello world";
 console.log(message);
+var Name = "John";
+var score1 = 50;
+var score2 = 42.50;
+var sum = score1 + score2;
+console.log("Name" + Name);
+console.log("first score: " + score1);
+console.log("second score: " + score2);
+console.log("sum of the scores: " + sum);
--- test.ts
+++ test.ts
@@ -1,2 +1,11 @@
 var message: string = "Hello world";
 console.log(message);
+
+var Name:string = "John"; 
+var score1:number = 50;
+var score2:number = 42.50
+var sum = score1 + score2 
+console.log("Name"+Name) 
+console.log("first score: "+score1) 
+console.log("second score: "+score2) 
+console.log("sum of the scores: "+sum)
